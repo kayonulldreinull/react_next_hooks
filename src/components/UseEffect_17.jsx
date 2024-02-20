1899c2b63e4c4f58d6dd88d080c30e70ff53ebfe
Kayo <everyzinn@gmail.com>
2024-02-20 17:00:26 -0300

    UseEffect _17

--- src/components/UseEffect_17.jsx
+++ src/components/UseEffect_17.jsx
@@ -3,20 +3,35 @@ import React, {useState, useEffect} from 'react'
 export default () => {
 
     const [valor, setValor] = useState(0)
+    const [nome, setNome] = useState()
 
     function incremento() {
         setValor(valor + 1)
     }
 
+    function definirNome() {
+        setNome('Kayo Barreto')
+    }
+
 useEffect(() => {
-    console.log('componente iniciado 01')
+    console.log('Executar apenas no inicio do componente')
 }, [])
+
+useEffect(() => {
+    console.log('Executar apenas na alteracao do Valor')
+}, [valor])
+
+useEffect(() => {
+    console.log('Executar apenas na alteracao do Nome')
+}, [nome])
 
     return (
         <>
                 <h3>UseEffect 01</h3>
                 <button onClick={incremento}>Incremento</button>
                 <h3>{valor}</h3>
+                <button onClick={definirNome}>Definir Nome</button>
+                <h3>{nome}</h3>
         </>
     )
 }
